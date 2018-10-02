3a5c154afd83b08dc374f58e2ccb02ee90b8d2e1
Gabriela Menocal <gmenocal89@gmail.com>
2018-10-01 23:30:35 -0400

    missing some parts

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bikes", "Sports",300, 10), ("Coffee Table", "Living Room Furniture",50, 50), ("Lenovo Carbono", "Computers",1500, 10), ("GF Cereal", "Grocery",3, 100);
+
+I
+
+-- ### Alternative way to insert more than one row
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
